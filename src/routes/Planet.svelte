6fa8cb26430b5c19a625c836ca4c3bdc03d85a5c
Safirl <loic.leforestier@mmibordeaux.com>
2025-05-11 19:42:37 +0200

    bug : planet is blue

--- src/routes/Planet.svelte
+++ src/routes/Planet.svelte
@@ -2,28 +2,46 @@
 	import { onMount } from 'svelte';
 	import * as THREE from 'three';
 	import GUI from 'lil-gui';
+	import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 
 	let container: HTMLDivElement;
 
 	onMount(() => {
+		/**
+		 * DEBUG Object
+		 */
+		const gui = new GUI();
+		const debugObject = {
+			color: '#000',
+			cameraFov: 75,
+			farClip: 1000
+		};
+
 		const scene = new THREE.Scene();
 		const camera = new THREE.PerspectiveCamera(
-			75,
+			debugObject.cameraFov,
 			container.clientWidth / container.clientHeight,
 			0.1,
-			0.905
+			debugObject.farClip
 		);
-		camera.position.z = 1.4;
+		const controls = new OrbitControls(camera, document.getElementById("container"))
+		camera.position.z = 5;
 		const renderer = new THREE.WebGLRenderer();
 		renderer.setSize(container.clientWidth, container.clientHeight);
 		container.appendChild(renderer.domElement);
 
-		const geometry = new THREE.SphereGeometry(1, 32, 32);
-		const material = new THREE.MeshDepthMaterial()
-		// const material = new THREE.MeshBasicMaterial({ color: '#3a6ea6', wireframe: true });
-		material.wireframe = true;
+		const geometry = new THREE.SphereGeometry(3, 32, 32);
+		// const material = new THREE.MeshDepthMaterial()
+		const material = new THREE.MeshBasicMaterial(/*{ color: 0xffffff, wireframe: false }*/);
+		const planeMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff, wireframe: false });
+		// planeMaterial.side = THREE.DoubleSide
 		const sphere = new THREE.Mesh(geometry, material);
-		scene.add(sphere);
+
+		const planeGeometry = new THREE.PlaneGeometry(2, 1)
+		const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+		plane.position.z += 3;
+
+		scene.add(sphere, plane);
 
 		scene.fog = new THREE.Fog("#3a6ea6", 0.1, 0.905);
 
@@ -55,17 +73,21 @@
 
 		animate();
 
-		//DEBUG
-		const gui = new GUI();
-		const debugObject = {
-			color: '#3a6ea6',
-			cameraFov: 75,
-			farClip: 0.905
-		};
+		/**
+		 * DEBUG FUNCTIONS
+		*/
+		// const gui = new GUI();
+		// const debugObject = {
+		// 	color: '#3a6ea6',
+		// 	cameraFov: 75,
+		// 	farClip: 1.519
+		// };
 		gui.add(camera.position, 'z', 0, 10, 0.01).name('cameraDistance');
-		gui.addColor(debugObject, 'color').onChange((newColor: string) => {
-			material.color.set(newColor);
-		});
+		// gui.addColor(debugObject, 'color').onChange((newColor: string) => {
+		// 	console.log(material.color)
+		// 	material.color.set(newColor);
+		// 	renderer.render(scene, camera);
+		// });
 		gui.add(debugObject, 'cameraFov', 0, 150, 0.01).onChange((newFov: number) => {
 			camera.fov = newFov;
 			camera.updateProjectionMatrix();
@@ -77,7 +99,7 @@
 	});
 </script>
 
-<div bind:this={container}></div>
+<div id="container" bind:this={container}></div>
 
 <style lang="scss">
 	div {
